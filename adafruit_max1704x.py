5f21bbdff10650a5c40c8c3cbe3e4259abae70fb
lady ada <limor@ladyada.net>
2022-08-06 19:40:35 -0400

    added some hibernation and alert stuff

--- adafruit_max1704x.py
+++ adafruit_max1704x.py
@@ -50,6 +50,13 @@ _MAX1704X_CHIPID_REG = const(0x19)
 _MAX1704X_STATUS_REG = const(0x1A)
 _MAX1704X_CMD_REG = const(0xFE)
 
+ALERTFLAG_SOC_CHANGE = 0x20
+ALERTFLAG_SOC_LOW = 0x10
+ALERTFLAG_VOLTAGE_RESET = 0x08
+ALERTFLAG_VOLTAGE_LOW = 0x04
+ALERTFLAG_VOLTAGE_HIGH = 0x02
+ALERTFLAG_RESET_INDICATOR = 0x01
+
 class MAX17048:
     """Driver for the MAX1704X battery fuel gauge.
     :param ~busio.I2C i2c_bus: The I2C bus the MAX1704X is connected to.
@@ -58,18 +65,28 @@ class MAX17048:
 
     chip_version = ROUnaryStruct(_MAX1704X_VERSION_REG, ">H")
     _config = ROUnaryStruct(_MAX1704X_CONFIG_REG, ">H")
+    # expose the config bits
+    sleep = RWBit(_MAX1704X_CONFIG_REG+1, 7, register_width=2, lsb_first=False)
+    _alert_status = RWBit(_MAX1704X_CONFIG_REG+1, 5, register_width=2, lsb_first=False)
+    enable_sleep = RWBit(_MAX1704X_MODE_REG, 5)
+    hibernating = ROBit(_MAX1704X_MODE_REG, 4)
+
     _cmd = UnaryStruct(_MAX1704X_CMD_REG, ">H")
     _status = ROUnaryStruct(_MAX1704X_STATUS_REG, ">B")
     _cell_voltage = ROUnaryStruct(_MAX1704X_VCELL_REG, ">H")
     _cell_SOC = ROUnaryStruct(_MAX1704X_SOC_REG, ">B")
     _hibrt_actthr = UnaryStruct(_MAX1704X_HIBRT_REG+1, ">B")
     _hibrt_hibthr = UnaryStruct(_MAX1704X_HIBRT_REG, ">B")
+    _valrt_min = UnaryStruct(_MAX1704X_VALERT_REG, ">B")
+    _valrt_max = UnaryStruct(_MAX1704X_VALERT_REG+1, ">B")
+
     # expose the alert bits
-    reset_alert = ROBit(_MAX1704X_STATUS_REG, 0)
-    voltage_high_alert = ROBit(_MAX1704X_STATUS_REG, 1)
-    voltage_low_alert = ROBit(_MAX1704X_STATUS_REG, 2)
-    voltage_reset_alert = ROBit(_MAX1704X_STATUS_REG, 3)
-    SOC_change_alert = ROBit(_MAX1704X_STATUS_REG, 5)
+    reset_alert = RWBit(_MAX1704X_STATUS_REG, 0)
+    voltage_high_alert = RWBit(_MAX1704X_STATUS_REG, 1)
+    voltage_low_alert = RWBit(_MAX1704X_STATUS_REG, 2)
+    voltage_reset_alert = RWBit(_MAX1704X_STATUS_REG, 3)
+    SOC_low_alert = RWBit(_MAX1704X_STATUS_REG, 4)
+    SOC_change_alert = RWBit(_MAX1704X_STATUS_REG, 5)
 
     chip_id = ROUnaryStruct(_MAX1704X_CHIPID_REG, ">B")
     
@@ -80,14 +97,18 @@ class MAX17048:
         if self.chip_version & 0xFFF0 != 0x0010:
             raise RuntimeError("Failed to find MAX1704X - check your wiring!")
         self.reset()
+        self.enable_sleep = False
+        self.sleep = False
 
     def reset(self):
         try:
             self._cmd = 0x5400    
         except OSError:
             # aha! we NACKed, which is CORRECT!
-            return self.reset_alert # we should have reset
-        raise RuntimeException("Reset did not succeed")
+            pass
+        else:
+            raise RuntimeError("Reset did not succeed")
+        self.reset_alert = False # clean up RI alert
 
     @property
     def cell_voltage(self):
@@ -96,6 +117,36 @@ class MAX17048:
     @property
     def cell_percent(self):
         return self._cell_SOC
+
+
+    @property
+    def voltage_alert_min(self):
+        return self._valrt_min * .02 # 20mV / LSB
+
+    @voltage_alert_min.setter
+    def voltage_alert_min(self, minvoltage):
+        if (not 0 <= minvoltage <= (255 * 0.02)):
+            raise ValueError("Alert voltage must be between 0 and 5.1 Volts")
+        self._valrt_min = int(minvoltage / .02) # 20mV / LSB
+
+    @property
+    def voltage_alert_max(self):
+        return self._valrt_max * .02 # 20mV / LSB
+
+    @voltage_alert_max.setter
+    def voltage_alert_max(self, maxvoltage):
+        if (not 0 <= maxvoltage <= (255 * 0.02)):
+            raise ValueError("Alert voltage must be between 0 and 5.1 Volts")
+        self._valrt_max = int(maxvoltage / .02) # 20mV / LSB
+
+    @property
+    def active_alert(self):
+        return self._alert_status
+
+    @property
+    def alert_reason(self):
+        return self._status & 0x3F
+
 
     @property
     def activity_threshold(self):
@@ -117,4 +168,20 @@ class MAX17048:
         if (not 0 <= threshold_percent <= (255 * 0.208)):
             raise ValueError("Activity percentage/hour change must be between 0 and 53%")
         self._hibrt_hibthr  = int(threshold_percent / 0.208)  # 0.208% per hour
+
+
+    def hibernate(self):
+        """Setup thresholds for hibernation to go into hibernation mode immediately.
+        See datasheet: HIBRT Register (0x0A) To disable hibernate mode, set HIBRT = 0x0000. To
+        always use hibernate mode, set HIBRT = 0xFFFF. Can check status with `self.hibernating`"""
+        self._hibrt_hibthr = 0xFF
+        self._hibrt_actthr = 0xFF
+
+    def disable_hibernation(self):
+        """Setup thresholds for hibernation to leave hibernation mode immediately.
+        See datasheet: HIBRT Register (0x0A) To disable hibernate mode, set HIBRT = 0x0000. To
+        always use hibernate mode, set HIBRT = 0xFFFF. Can check status with `self.hibernating`"""
+        self._hibrt_hibthr = 0
+        self._hibrt_actthr = 0
+
 
--- examples/max1704x_simpletest.py
+++ examples/max1704x_simpletest.py
@@ -19,11 +19,50 @@ print("Found MAX1704x with chip version", hex(max17.chip_version))
 #max17.activity_threshold = 0.15
 #print("MAX1704x activity threshold = %0.2f V" % max17.activity_threshold)
 
-max17.hibernation_threshold = 5
-print("MAX1704x hibernation threshold = %0.2f %%" % max17.hibernation_threshold)
+#max17.hibernation_threshold = 5
+#print("MAX1704x hibernation threshold = %0.2f %%" % max17.hibernation_threshold)
 
+#max17.hibernate()
+
+#max17.enable_sleep = True
+#max17.sleep = True
+
+max17.voltage_alert_min = 3.7
+print("Voltage alert minimum = %0.2f V" % max17.voltage_alert_min)
+#max17.voltage_alert_max = 3.8
+#print("Voltage alert maximum = %0.2f V" % max17.voltage_alert_max)
 
 while True:
     print("Battery voltage:", max17.cell_voltage, "V")
     print("Battery state  :", max17.cell_percent, "%")
+
+    if max17.hibernating:
+        print("Hibernating!")
+
+    if max17.active_alert:
+        print("Alert!")
+        if max17.reset_alert:
+            print("  Reset indicator")
+            max17.reset_alert = False # clear the alert
+
+        if max17.voltage_high_alert:
+            print("  Voltage high")
+            max17.voltage_high_alert = False # clear the alert
+
+        if max17.voltage_low_alert:
+            print("  Voltage low")
+            max17.voltage_low_alert = False # clear the alert
+
+        if max17.voltage_reset_alert:
+            print("  Voltage reset")
+            max17.voltage_reset_alert = False # clear the alert
+
+        if max17.SOC_low_alert:
+            print("  Charge low")
+            max17.SOC_low_alert = False # clear the alert
+
+        if max17.SOC_change_alert:
+            print("  Charge changed")
+            max17.SOC_change_alert = False # clear the alert
+
     time.sleep(1)
