3c98b1ea6a8d9b71f0f4d65e3d916ff0e801d3b8
Alec Delaney <tekktrik@gmail.com>
2022-08-08 19:13:53 -0400

    Reformatted per pre-commit

--- setup.py
+++ setup.py
@@ -57,8 +57,7 @@ setup(
     ],
     # What does your project relate to?
     keywords="adafruit blinka circuitpython micropython max1704x max17048 max17049 battery "
-             "lipoly lipo fuel gauge monitor",
-
+    "lipoly lipo fuel gauge monitor",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
