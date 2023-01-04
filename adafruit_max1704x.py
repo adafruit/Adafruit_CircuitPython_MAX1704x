2f4dcfd9d21953d5407327120cadf44185cb7235
Jeff Epler <jepler@gmail.com>
2023-01-04 12:29:25 -0600

    fix pylint errors

--- adafruit_max1704x.py
+++ adafruit_max1704x.py
@@ -128,12 +128,11 @@ class MAX17048:
             try:
                 self.reset_alert = False  # clean up RI alert
                 return
-            except OSError as e:
+            except OSError:
                 # With CircuitPython 8.0.0-beta.6 and ESP32-S3, the first
                 # attempt to reset the alert fails.
                 continue
-        else:
-            raise RuntimeError("Clearing reset alert did not succeed")
+        raise RuntimeError("Clearing reset alert did not succeed")
 
     @property
     def cell_voltage(self) -> float:
